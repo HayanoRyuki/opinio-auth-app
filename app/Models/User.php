5cebb31512413268dd085aa7ee8f79a3e0a197f7
HayanoRyuki <hayano@media-confidence.com>
2026-01-01 08:02:15 +0900

    Add clients relation to User model for SSO

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,10 +2,10 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use App\Models\Client;
 
 class User extends Authenticatable
 {
@@ -21,6 +21,8 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'company_id',
+        'role',
     ];
 
     /**
@@ -44,5 +46,19 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    /**
+     * Client membership (client_user)
+     * SSO / 権限制御用
+     */
+    public function clients()
+    {
+        return $this->belongsToMany(
+            Client::class,
+            'client_user',
+            'user_id',
+            'client_id'
+        )->withPivot('role');
     }
 }
